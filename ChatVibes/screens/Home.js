076a9b4d0113bb5a14a1ac1e47c8a30570bdb2d9
robbiedantonio <robbie.dantonio@gmail.com>
2023-09-17 15:57:20 -0400

    Added new button to open User list page

--- ChatVibes/screens/Home.js
+++ ChatVibes/screens/Home.js
@@ -1,9 +1,16 @@
+/**
+ * Home screen, which is first screen
+ * that user sees after signing in.
+ * 
+ * This can probably be reworked a bit for simplicity!
+ */
+
 import React, { useEffect } from "react";
 import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 import colors from '../colors';
-import { Entypo } from '@expo/vector-icons';
+//import { Entypo } from '@expo/vector-icons';
 const ImageUrl = "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
 const Home = () => {
 
@@ -12,8 +19,15 @@ const Home = () => {
     useEffect(() => {
         navigation.setOptions({
             headerLeft: () => (
-                <FontAwesome name="search" size={24} color={colors.gray} style={{marginLeft: 15}}/>
-            ),
+                <View style={styles.container}>
+            <TouchableOpacity
+                onPress={() => navigation.navigate("Users")}
+                style={styles.userListButton}
+            >
+                <MaterialCommunityIcons name="account-search" size={24} color="white" />
+            </TouchableOpacity>
+        </View>
+        ),
             headerRight: () => (
                 <Image
                     source={{ uri: ImageUrl }}
@@ -47,6 +61,23 @@ const Home = () => {
             justifyContent: 'flex-end',
             alignItems: 'flex-end',
             backgroundColor: "#fff",
+        },
+        userListButton: {
+            backgroundColor: colors.primary2,
+            height: 40,
+            width: 40,
+            borderRadius: 25,
+            alignItems: 'center',
+            justifyContent: 'center',
+            shadowColor: colors.primary2,
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: .9,
+            shadowRadius: 8,
+            marginLeft: 20,
+            marginBottom: 5,
         },
         chatButton: {
             backgroundColor: colors.primary,
